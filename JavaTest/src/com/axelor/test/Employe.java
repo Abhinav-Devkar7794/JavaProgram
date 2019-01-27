44371f0cf98b7d7ddce2835efe8a5d6df03f24b7
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-27 09:05:47 +0530

    Commit second project

---
+++ JavaTest/src/com/axelor/test/Employe.java
@@ -0,0 +1,13 @@
+package com.axelor.test;
+
+public class Employe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Second Project !!!");
+		
+	}
+
+}
