1c38993a91919d0b58673be91b8164e0ac64c38b
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-27 07:58:22 +0530

    Add Studen Classs

---
+++ Java8Features/src/com/axelor/java8/Student.java
@@ -0,0 +1,15 @@
+package com.axelor.java8;
+
+public class Student {
+
+	static void message() {
+		System.out.println("My First Message !!!");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		message();
+	}
+
+}
